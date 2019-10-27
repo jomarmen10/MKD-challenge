fab269d824de632c22965dc1f89f93bb8b011652
Jomar Mendoza <jmm21@Jomars-MBP.home>
2019-10-27 01:36:48 -0700

    added the input area and onchage function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,11 +7,21 @@ class Form extends Component {
     message: ''
   }
 
+  handleChange = e => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+  }
 
   render(){
+    const { name, email, message } = this.state
     return(
       <div>
-        form connected
+        <form>
+          <input name="name" value={name} onChange={this.handleChange}></input>
+          <input name="email" value={email} onChange={this.handleChange}></input>
+          <input name="message" value={message} onChange={this.handleChange}></input>
+        </form>
       </div>
     )
   }
