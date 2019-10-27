b5b8cf9f09fceed2245ee72284b7fe7b9de495ad
Jomar Mendoza <jmm21@Jomars-MBP.home>
2019-10-27 01:38:20 -0700

    created the submit fucntion and added the button to the form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -13,14 +13,20 @@ class Form extends Component {
     })
   }
 
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log('submit working')
+  }
+
   render(){
     const { name, email, message } = this.state
     return(
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input name="name" value={name} onChange={this.handleChange}></input>
           <input name="email" value={email} onChange={this.handleChange}></input>
           <input name="message" value={message} onChange={this.handleChange}></input>
+          <button>send</button>
         </form>
       </div>
     )
