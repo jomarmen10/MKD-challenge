7c7fa23877c179cc8971120380c2bebdff323c89
Jomar Mendoza <jmm21@Jomars-MBP.home>
2019-10-27 02:07:56 -0700

    added the div image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Form from './components/Form/Form';
+import './App.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Contact Form</h1>
-      <Form/>
+    <div className="container">
+      <div className="center">
+        <div className="left">
+        </div>
+        <div className="right">
+          <div className="form-container">
+            <h1>Contact Form</h1>
+            <Form/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
